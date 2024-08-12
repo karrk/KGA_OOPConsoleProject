e957c65514a71840020f0883bc78e36c38b3e9bb
karrk <101902544+karrk@users.noreply.github.com>
2024-08-12 09:43:53 +0900

    [Create] MiniCooked 프로젝트 빌드

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniCooked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
