4403269a14f4d47d561127335a8d91a3c6cc8afd
karrk <101902544+karrk@users.noreply.github.com>
2024-08-12 16:08:22 +0900

    [Feat] : 레이아웃 클래스, 텍스트박스 생성

---
+++ Layout.cs
@@ -0,0 +1,119 @@
+﻿using MiniCooked;
+
+public class Layout
+{
+    private int _posX;
+    private int _posY;
+
+    private int _width;
+    private int _height;
+
+    public int Top => _posY;
+    public int Left => _posX;
+    public int Right => _posX + _width;
+    public int Bottom => _posY + _height;
+
+    public int Width => _width;
+    public int Height => _height;
+
+    private int _sortingOrder;
+
+    private List<Layout> _innerLayouts;
+    private List<TextBox> _textBoxes;
+
+    public Layout(int m_startX, int m_startY, int m_width, int m_height)
+    {
+        this._posX = m_startX;
+        this._posY = m_startY;
+
+        this._width = m_width;
+        this._height = m_height;
+    }
+
+    public void AddLayout(Layout m_layout)
+    {
+        if (_innerLayouts == null)
+            _innerLayouts = new List<Layout>();
+
+        _innerLayouts.Add(m_layout);
+    }
+
+    public void AddLayout(int m_startX, int m_startY, int m_width, int m_height)
+    {
+        if (_innerLayouts == null)
+            _innerLayouts = new List<Layout>();
+
+        _innerLayouts.Add(new Layout(m_startX, m_startY, m_width, m_height));
+    }
+
+    public void SetSortingOrder(int m_sort)
+    {
+        this._sortingOrder = m_sort;
+    }
+
+
+
+    public void AddText(TextBox m_text)
+    {
+        if (_textBoxes == null)
+            _textBoxes = new List<TextBox>();
+
+        _textBoxes.Add(m_text);
+        m_text.ParentLayout = this;
+    }
+
+    public void PrintLayout()
+    {
+        Console.SetCursorPosition(_posX, _posY);
+
+
+        //상단 테두리
+        Console.Write(Fonts.LAYOUT_OUTLINE_TOPLEFT);
+
+        for (int i = _posX; i < _posX + _width - 2; i++)
+        {
+            Console.Write(Fonts.LAYOUT_OUTLINE_HORIZON);
+        }
+
+        Console.WriteLine(Fonts.LAYOUT_OUTLINE_TOPRIGHT);
+
+
+        //좌우
+        for (int i = _posY + 1; i < _posY + _height; i++)
+        {
+            Console.SetCursorPosition(_posX, i);
+            Console.Write(Fonts.LAYOUT_OUTLINE_VERTICAL);
+            Console.SetCursorPosition(_posX + _width - 1, i);
+            Console.Write(Fonts.LAYOUT_OUTLINE_VERTICAL);
+        }
+
+        //하단부
+        Console.SetCursorPosition(_posX, _height + _posY);
+
+        Console.Write(Fonts.LAYOUT_OUTLINE_DOWNLEFT);
+
+        for (int i = _posX; i < _posX + _width - 2; i++)
+        {
+            Console.Write(Fonts.LAYOUT_OUTLINE_HORIZON);
+        }
+
+        Console.Write(Fonts.LAYOUT_OUTLINE_DOWNRIGHT);
+
+        if (_innerLayouts != null)
+        {
+            for (int i = 0; i < _innerLayouts.Count; i++)
+            {
+                _innerLayouts[i].PrintLayout();
+            }
+        }
+
+        if (_textBoxes != null)
+        {
+            for (int i = 0; i < _textBoxes.Count; i++)
+            {
+                _textBoxes[i].PrintText();
+            }
+        }
+
+    }
+}
---
+++ TextBox.cs
@@ -0,0 +1,113 @@
+﻿using System.Text;
+
+public class TextBox
+{
+    public enum TextHorizonAlign
+    {
+        Left,
+        Center,
+        Right,
+    }
+
+    public enum TextVerticalAlign
+    {
+        Top,
+        Center,
+        Bottom,
+    }
+
+    private StringBuilder _sb;
+    public int Length => _sb.Length;
+
+    private int _posX;
+    private int _posY;
+
+    private int _printLine;
+
+    public int PrintLine => _printLine;
+
+    private TextHorizonAlign _horizonAlign;
+    private TextVerticalAlign _verticalAlign;
+
+    public Layout ParentLayout;
+
+    public TextBox(string m_text)
+    {
+        this._sb = new StringBuilder();
+        this._sb.Append(m_text);
+    }
+
+    public void AddText(string m_text, bool m_lineMode = false)
+    {
+        if (m_lineMode)
+            _sb.AppendLine();
+
+        _sb.Append(m_text);
+    }
+
+    public void SetNewText(string m_text)
+    {
+        _sb.Clear();
+        _sb.Append(m_text);
+    }
+
+    public void SetPos(int m_posX, int m_posY)
+    {
+        this._posX = m_posX;
+        this._posY = m_posY;
+    }
+
+    public void SetLine(int m_line)
+    { this._printLine = m_line; }
+
+    public void SetAlign(TextHorizonAlign m_horizon)
+    {
+        this._horizonAlign = m_horizon;
+    }
+
+    public void SetAlign(TextVerticalAlign m_vertical)
+    {
+        this._verticalAlign = m_vertical;
+    }
+
+    public void PrintText()
+    {
+        int row = ParentLayout.Left + 1;
+        int column = ParentLayout.Top + _printLine + 1;
+
+
+        switch (_horizonAlign)
+        {
+            case TextHorizonAlign.Center:
+                row = ParentLayout.Right - (ParentLayout.Width / 2) - Length / 2;
+                break;
+            case TextHorizonAlign.Right:
+                row = ParentLayout.Right - Length * 2 - 1;
+                break;
+        }
+
+        switch (_verticalAlign)
+        {
+            case TextVerticalAlign.Center:
+                column = ParentLayout.Bottom - (ParentLayout.Height / 2);
+                break;
+            case TextVerticalAlign.Bottom:
+                column = ParentLayout.Bottom - _printLine - 1;
+                break;
+        }
+
+        Console.SetCursorPosition(row, column);
+
+        for (int i = 0; i < _sb.Length; i++)
+        {
+            if (_sb[i] == '\n')
+            {
+                Console.WriteLine();
+                Console.SetCursorPosition(row, ++column);
+                continue;
+            }
+
+            Console.Write(_sb[i]);
+        }
+    }
+}
