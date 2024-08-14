4f013e729cd3bc8b0a7f3a56f05e50bd55bb07b1
karrk <101902544+karrk@users.noreply.github.com>
2024-08-14 20:40:17 +0900

    [Feat] : 커스터머컨테이너 클래스
    방문한 손님의 주문목록을 관리하기 위한 클래스
    제시된 음식을 주문목록 중 일치하는 음식이 있는지 확인합니다.

---
+++ CustomerContainer.cs
@@ -0,0 +1,55 @@
+﻿using MiniCooked;
+
+public static class CustomerContainer
+{
+    private static LinkedList<Customer> _customers = new LinkedList<Customer>();
+    private static Thread _thread;
+    private static Random _rand;
+
+    public static void AddCustomer(Customer m_customer)
+    {
+        _customers.AddLast(m_customer);
+    }
+
+    public static void RemoveCustomer(Customer m_customer)
+    {
+        LinkedListNode<Customer> node = _customers.First;
+
+        while (true)
+        {
+            if (node == null)
+                throw new Exception("찾을 수 없는 노드");
+
+            else if(node.Value.Equals(m_customer))
+            {
+                node.Value.Hide();
+                _customers.Remove(m_customer);
+                break;
+            }
+                
+            node = node.Next;
+        }
+    }
+
+    public static void SearchBurger(int m_foodTotalNumber)
+    {
+        LinkedListNode<Customer> node = _customers.First;
+
+        while (true)
+        {
+            if (node == null)
+                break;
+            else if(MenuManager.GetBurgerTotalNumber(node.Value.SelectNumber) == m_foodTotalNumber)
+            {
+                GameManager.Instance.AddGold(
+                    MenuManager.Burgers[node.Value.SelectNumber-1].Price);
+
+                RemoveCustomer(node.Value);
+                break;
+            }
+
+            node = node.Next;
+        }
+    }
+
+}
