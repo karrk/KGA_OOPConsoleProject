cabee9a0175c11b395dfb41238937fac1ca2103c
karrk <101902544+karrk@users.noreply.github.com>
2024-08-15 11:50:59 +0900

    [Refactor] : 클래스, Enum 분리

---
+++ CharController.cs
@@ -0,0 +1,21 @@
+﻿public static class CharController
+{
+    public static char HalfToFull(char m_half)
+    {
+        if (m_half > 0x21 && m_half <= 0x7e)
+
+            m_half += (char)0xfee0;
+
+        else if (m_half == 0x20)
+
+            m_half = (char)0x3000;
+
+        return m_half;
+    }
+
+    public static bool isHalf(char m_half)
+    {
+        return (m_half >= 0x20 && m_half <= 0x7E);
+    }
+}
+
---
+++ Enums.cs
@@ -0,0 +1,27 @@
+﻿public enum HorizonAlign
+{
+    Left,
+    Center,
+    Right,
+}
+
+public enum VerticalAlign
+{
+    Top,
+    Center,
+    Bottom,
+}
+
+public enum RectOption
+{
+    Relative,
+    Absolute,
+}
+
+public enum RectCorner
+{
+    TopL,
+    TopR,
+    BotL,
+    BotR,
+}
